1f99326aa30ef2eaef9bd2175edc73a73ffbe0b7
Binapi-Consaltic <zelina@elektrozelina.eu>
2022-12-26 23:16:49 +0100

    prodal jsem do projektu footer.php

--- layouts/footer.php
+++ layouts/footer.php
@@ -1,3 +1,67 @@
-<div class="container">
-    <p class="m-0 text-center text-white">Copyright &copy; <?php echo $bussinessName; ?> <?php echo "20".date("y"); ?> email: info@zelina.tech tel: 732 127 410</p>
-</div>
+<!DOCTYPE html>
+<html lang="cs">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Favicon-->
+    <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+    <!-- Bootstrap icons-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
+    <!-- Core theme CSS (includes Bootstrap)-->
+    <link href="../css/styles.css" rel="stylesheet" type="text/css" />
+    <title></title>
+</head>
+<body>
+    <footer>
+        <section class="app_footer_navigation">
+            <div class="row">
+                <div class="col-sm-4">
+                    <h2>Reklamace</h2>
+                    <ul class="footer-ul">
+                        <li><a href="">Jak a kde reklamovat</a></li>
+                        <li><a href="">Reklamační řád</a></li>
+                    </ul>
+                </div>
+                <div class="col-sm-4">
+                    <h2>Služby</h2>
+                    <ul class="footer-ul">
+                        <li><a href="">www.zelina.tech</a></li>
+                        <li><a href="">Finanční služby</a></li>
+                        <li><a href="">Outsourcing dopravy a logistiky</a></li>
+                    </ul>
+                </div>
+                <div class="col-sm-4">
+                    <h2>O nás</h2>
+                    <ul class="footer-ul">
+                        <li><a href="">Obchodní podmínky</a></li>
+                        <li><a href="">Zpracování osobních údajů</a></li>
+                        <li><a href="">Profil společnosti</a></li>
+                        <li><a href="">Kontak</a></li>
+                        <li><a href="">Kariéra</a></li>
+                    </ul>
+                </div>
+            </div>
+        </section>
+        <section class="app_footer_image">
+            <div class="row">
+                <div class="col footer-image">
+                    <img src="../images/footer/hp21.png" alt="">
+                </div>
+                <div class="col footer-image">
+                    <img src="../images/footer/czechTOP-cz.png" alt="">
+                </div>
+                <div class="col footer-image">
+                    <img src="../images/footer/canon21.png" alt="">
+                </div>
+                <div class="col footer-image">
+                    <img src="../images/footer/Broadline 2021.pngC:/xampp/htdocs/zelina.tech/layouts/menu.php" alt="">
+                </div>
+                <div class="col footer-image">
+                    <img src="../images/footer/aaa_duveryhodnost.png" alt="">
+                </div>
+            </div>
+        </section>
+    </footer>
+</body>
+</html>
