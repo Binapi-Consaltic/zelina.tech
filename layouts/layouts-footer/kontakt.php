e25bd321b68bf3f6592ec5df48fc35d7ea4aa7f2
Binapi-Consaltic <zelina@elektrozelina.eu>
2022-12-27 22:16:38 +0100

    Upravil jsem paticku v projektu

---
+++ layouts/layouts-footer/kontakt.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Favicon-->
+    <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+    <!-- Bootstrap icons-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
+    <!-- Core theme CSS (includes Bootstrap)-->
+    <link href="css/styles.css" rel="stylesheet" type="text/css" />
+    <title>Kontakt | <?php echo $bussinessName; ?></title>
+</head>
+<body>
+    <header><?php require_once("../../layouts/nav-menu.php"); ?></header>
+</body>
+</html>
