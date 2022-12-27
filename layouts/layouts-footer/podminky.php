b6b724698cf0d24ced52216718478357d9e6f8dc
Binapi-Consaltic <zelina@elektrozelina.eu>
2022-12-27 23:26:25 +0100

    pridal jsem do projektu obxchodní podminky

---
+++ layouts/layouts-footer/podminky.php
@@ -0,0 +1,104 @@
+<?php
+
+require_once("../../setup/connect.php");
+
+?>
+<!DOCTYPE html>
+<html lang="cs">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Favicon-->
+    <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+    <!-- Bootstrap icons-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
+    <!-- Core theme CSS (includes Bootstrap)-->
+    <link href="../../css/styles.css" rel="stylesheet" type="text/css" />
+    <title>Obchodní podmínky | <?php echo $bussinessName; ?></title>
+</head>
+<style>
+    h1{
+        margin-top: 2%;
+    }
+</style>
+<body>
+    <header><?php require_once("../nav-menu.php"); ?></header>
+    <main class="layout">
+        <h1>Obchodní podmínky</h1>
+
+        
+        <p>MILLENIUM INTERNATIONAL s.r.o. se orientuje na distribuční formu prodeje, která je realizována přes rozsáhlé sítě resellerů. Obchodními partnery firmy jsou specializovaní dealeři, VAR (value-added reseller), systémoví integrátoři, prodejny s výpočetní technikou a právnické a fyzické osoby zapsané v obchodním rejstříku, které nemají svou činnost pozastavenou nebo přerušenou, neprodáváme tedy koncovým zákazníkům.</p>
+
+
+        <h3>DEALERSKÉ SMLOUVY (První obchodní kontakt)<h3>
+        <p>Smlouva o obchodních vztazích,Všeobecné obchodní podmínky a Reklamační řád upravují dodavatelsko - odběratelské vztahy mezi dealerem (kupujícím) a prodávajícím (firmou MILLENIUM INTERNATIONAL s.r.o.). Pokud projevíte zájem stát se našim dealerem, obraťte se na pracovníky obchodního oddělení, kteří Vám zašlou návrh Smlouvy o obchodních vztazích s aktuálním nabídkovým ceníkem celého sortimentu zboží. Tyto smluvní dokumenty najdete taktéž na www.edsystem.cz. Pokud se rozhodnete s námi Smlouvu o obchodních vztazích uzavřít, zašlete nám laskavě vyplněné smlouvy spolu s výpisem z obchodního rejstříku, příp. živnostenským listem. Při vyplňování obchodního jména kupujícího dbejte na to, aby toto obchodní jméno odpovídalo obchodnímu jménu uvedenému v obchodním rejstříku, příp. živnostenském listě. V souvislosti s tím si Vás dovolujeme požádat o zaslání základních informací o Vaší firmě - firemní profil, prodávaný sortiment, ceník, aj. Obratem obdržíte námi potvrzené vyhotovení smlouvy. V případě zájmu o zaslání formulářů dealerských smluv se spojte s těmito kontaktními osobami:<br><br>
+
+        Ondřej Zelina - info@zelina.tech.<br><br>
+
+
+        Obchodní asistentka příjímá a vyřizuje Vaše objednávky. Při telefonickém kontaktu Vás informuje o příjmu Vaší objednávky, stavu Vaší zakázky (blokace zboží, vystavení proforma faktury, faktury), zboží na cestě ke klientovi, nedodaném zbožím, řeší se zákazníky finanční otázky, tj. sleduje platební bilance, informuje dealery o neuhrazených fakturách po době splatnosti, atd.<br>
+
+        Obchodní manažeři Vám poskytnou tyto informace: základní technické informace o nabízených produktech, popř. možnosti náhradních řešení aktuální ceny k daným produktům, informace o stavu skladu daného produktu, sestavení speciální cenové nabídky na větší objem zboží, informace o obchodních podmínkách (dealer. kategorie, způsoby plateb, dopravy aj.). Taktéž se na naše obch. manažery obraťte v případě zájmu o zaslání informačních materiálů o konkrétních produktech a veškerých dalších materiálů.<br>
+
+        Kredit manažeři řídí tok finančních prostředků k našim odběratelům. Jedná se především o platební a kreditní politiku. Obracejte se na ně s Vašimi požadavky na udělení kreditu pro odběry zboží na fakturu, způsoby jištění tohoto kreditu a s požadavky na profinancování větších zakázek, udělování delší doby splatnosti.</p><br><br>
+
+
+        <h3>CENY</h3>
+        Kupní ceny zboží zveřejňuje prodávající v ceníku, který je kupujícímu přístupný na internetové adrese www.edsystem.cz. Prodávájící si vyhrazuje právo provádět změny vyhlášených kupních cen. Při větších jednorázových odběrech zboží lze sjednat individuální cenové a platební podmínky. V tomto případě se obraťte na vedoucí jednotlivých pracovišť, viz Kontakty.<br><br>
+
+
+        <h3>OBJEDNÁVÁNÍ ZBOŽÍ</h3>
+        Objednávat zboží u prodávajícího lze následujícími způsoby:<br>
+
+        eDlinkem - elektronický objednávkový systém - www.zelina.tech<br>
+        e-mailem - objednávky zasílejte na konkrétní e-mailové adresy jednotlivých pracovníků obchodního oddělení, viz. <a href="kontak.php">Kontakty</a><br>
+        faxovou formou - na faxová čísla jednotlivých pracovišť (Praha), viz. <a href="kontakt.php">Kontakty</a>
+        písemnou formou<br>
+        osobně - na pracovišti Ostrava, viz. Kontakty - lze realizovat i okamžitý odběr zboží<br>
+        Telefonicky konzultované objednávky (blokované zboží) je nutné následně potvrdit písemnou formou nebo faxem.<br>
+
+        Pro maximální urychlení zpracování Vaší objednávky uvádějte, prosím, na všech objednávkách následující údaje (viz formulář objednávky):<br>
+
+        obchodní jméno a sídlo kupujícího<br>
+        jeho IČ a DIČ, pokud je registrován jako plátce DPH<br>
+        kód zboží dle ceníku<br>
+        přesný název zboží<br>
+        požadované množství<br>
+        jednotkovou dealerskou cenu<br>
+        přepravní dispozice<br>
+        obchodní jméno a podpis oprávněného zástupce kupujícího (faxová nebo písemná forma)<br>
+        V případě výrazné změny kurzu nebo ceny dodavatelem si vyhrazuje MILLENIUM INTERNATIONAL s.r.o., právo na následnou úpravu cen, o které budete dopředu informováni. Na základě řádné objednávky kupujícího vystaví prodávající tiskopis - potvrzení objednávky, v němž potvrzuje kupujícímu druh, předpokládanou cenu a množství produktů, jež se zavazuje kupujícímu dodat. Doporučujeme dealerům zkontrolovat jednotlivé položky objednávky. Předejde tím případným následným nedorozuměním.<br><br>
+
+
+        <h3>PLATEBNÍ PODMÍNKY</h3>
+        <p>Platby za odebírané zboží lze realizovat následujícími způsoby:<br>
+
+        platba na proforma fakturu - před dodávkou zboží. Platba se realizuje prostřednictvím proforma faktury, která je výzvou k provedení zálohové platby na náš účet (pod variabilním symbolem čísla proforma faktury). V okamžiku, kdy je zboží připraveno k expedici, je Vám vystavena (zafaxována, zaslána e-mailem) a následně zaslána poštou proforma faktura. Proforma fakturu si lze i on-line vystavit na www.edsystem.cz. Od okamžiku vystavení proforma faktury je Vám zboží blokováno na skladě po dobu splatnosti proforma faktury. Pokud přijmeme Vaši platbu během splatnosti proforma faktury, je zboží ihned expedováno. K zásilce je přiložen daňový doklad - faktura, na které je uvedeno, že částka byla zálohově uhrazena. Pokud není úhrada proforma faktury provedena v době splatnosti, blokace zboží pro Vaši objednávku je zrušena a vystavujete se tím nebezpečí prodlení dodávky. Podkladem pro expedici zboží u proforma faktur je fyzické naběhnutí částky na účet prodávajícího.<br>
+        platba v rámci kreditu - s odloženou splatností. Se stálými obchodními partnery je možné sjednat obchodní úvěr (kredit). Do výše tohoto kreditu - kredit limitu odebírá firma zboží na fakturu a v dohodnuté době splatnosti hradí částky faktur. Výše kredit limitu závisí na velikosti obratů za předcházející období a na platební morálce klientů a finančních výsledcích firmy. Individuálně stanovený kredit musí být kryt. Navrhujeme krytí směnkou, bankovní zárukou, popř. jinými individuálními mechanismy. Tyto podmínky jsou smluvně zakotveny v dodatku Smlouvy o obchodních vztazích. V případě zájmu o otevření kredit limitu se obracejte na kredit manažery jednotlivých pracovišť, viz. Kontakty.
+        Ve spolupráci s KB Vám nabízíme službu on-line plateb prostřednictvím www.edsystem.cz. Informace o provedené platbě jsme schopni zjistit během toho samého dne, kdy byla platba provedena z Vaší strany. Tato služba Vám zaručuje expedici v den platby a omezení manipulace s hotovými penězi. Podmínkou je, že platby, zadané jako on-line platby budou v těchto systémech KB:
+
+    
+        na účet MILLENIUM INTERNATIONAL s.r.o. IBAN: LT74 3250 0865 7950 0488 BIC: REVOLT21. Pokud budete chtít platit přes www.zelina.tech, sdělte MILLENIUM INTERNATIONAL s.r.o. číslo Vašeho účtu u Cašeho poskytovale banky. Bližší informace získáte u jednotlivých kredit manažerů, viz. Kontakty.<br><br>
+
+        <h3>Finanční služby</h3>
+        <p>V rámci obchodního úvěru lze sjednat delší splatnost, než je individuálně sjednána v rámci kreditu. Toto lze využít na základě finančních služeb poskytovaných firmou MILLENIUM INTERNATIONAL s.r.o. Jedná se o navýšení částky jednotlivých produktů na faktuře o individuálně sjednaný úrok za prodlouženou dobu splatnosti. Maximální doba splatnosti je stanovena na 30 kalendářních dnů. Standardní úrok za prodlouženou splatnost je stanoven na 0,04% denně a je upravován dle aktuálních úrokových sazeb komerčních bank. Velikost úroku a maximální dobu splatnosti lze individuálně sjednat s kredit manažerem, finančním manažerem, viz. **V případě doručení zboží dobírkou České Pošty jsou peníze disponovány na účet definovaný klientem  systemu do 1-2 dní od obdržení platby od České Pošty (obvykle 9-12 dní po přezetí zboží od České Pošty)<br>
+
+        Garance vrácení poplatků v plné výši při nedodržení smluvního času dodání. Vztahuje se pro objednávky nad 10 000 Kč.</p><br><br>
+
+
+        <h3>OSOBNÍ ODBĚR</h3>
+        <p>Osobní odběr zboží je možné realizovat pouze v rámci pracoviště Ostrava. Cena za balné je stanovena na 25,- Kč (vztahuje se vždy na objednávku). Informace on-line o stavech skladu najdete na www.edsystem.cz, nebo telefonicky zjistíte u jednotlivých pracovníků obchodního oddělení. Upozorňujeme zákazníky, že je třeba při osobních odběrech zboží na kredit (následná platba na fakturu v době splatnosti) mít uzavřený dodatek Smlouvy o obchodních vztazích týkající se osobních odběrů. V rámci tohoto dodatku jsou dealerem stanoveny oprávněné osoby k osobnímu odběru. Pouze tyto osoby budou oprávněny provádět osobní odběr zboží na fakturu v rámci kreditu.<br>
+
+        Těšíme se na naši budoucí spolupráci.<br><br><br>
+
+
+
+        <h3>ZPĚTNÝ ODBĚR VYSLOUŽILÝCH ELEKTROZAŘÍZENÍ / BATERIÍ</h3>
+        <p>Informace o odběru těchto zařízení najdete na stránce <a href="">zpětný odběr elektro</a></p>
+    </main>
+    <footer>
+        <?php require_once("../footer.php"); ?>
+    </footer>
+</body>
+</html>
